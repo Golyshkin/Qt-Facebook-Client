e4be4d5863342dc1eb369538da25e05a185f2c55
Alexander Golyshkin <Alexander.Golyshkin@gmail.com>
2011-05-07 11:12:57 +0400

    Added couple comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,14 @@ int main( int argc, char *argv[] )
 {
    QApplication a( argc, argv );
 
+   // Remove these rows if you don't have proxy server
    QNetworkProxy proxy;
-   proxy.setType(QNetworkProxy::HttpProxy);
-   proxy.setHostName("proxy.telma.nn");
-   proxy.setPort(3128);
-   QNetworkProxy::setApplicationProxy(proxy);
+   proxy.setType( QNetworkProxy::HttpProxy );
+   proxy.setHostName( "proxy.telma.nn" );
+   proxy.setPort( 3128 );
+   QNetworkProxy::setApplicationProxy( proxy );
 
    DialogManager manager;
-//test
+
    return a.exec();
 }
